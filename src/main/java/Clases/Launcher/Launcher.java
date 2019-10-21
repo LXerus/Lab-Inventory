b1ca819fc6a3d9a5679a4733cc9faf0c9e56d701
Cristhian Franco <crodfranco@gmail.com>
2019-10-20 19:24:32 -0600

    Adding documentation to the launcher class

--- src/main/java/Clases/Launcher/Launcher.java
+++ src/main/java/Clases/Launcher/Launcher.java
@@ -1,5 +1,10 @@
 package Clases.Launcher;
-//Launcher class created for Maven7
+/**
+ *<h1>Launcher class</h1>
+ * <p> The launcher purpose is for Maven to recognize the Main class and
+ * the javaFX framework.</p> 
+ * 
+ */
 public class Launcher {
     public static void main(String[] args){
         Main.main(args);
