e88a5c4cb842a3a08087d11a394caef7847be014
Cristhian Franco <crodfranco@gmail.com>
2019-10-20 18:44:55 -0600

    Testing bim message

--- src/main/java/Clases/Launcher/Launcher.java
+++ src/main/java/Clases/Launcher/Launcher.java
@@ -1,5 +1,5 @@
 package Clases.Launcher;
-//Launcher class created for Maven
+//Launcher class created for Maven7
 public class Launcher {
     public static void main(String[] args){
         Main.main(args);
